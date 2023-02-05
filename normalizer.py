c41f7c52f8aeb5cde058329f5cf0037d6285f145
yosefbeder <dryosefbeder@gmail.com>
2023-02-05 07:50:41 +0200

    Initial commit

---
+++ normalizer.py
@@ -0,0 +1,30 @@
+import re
+
+from tokenizer import tokenize
+from constants import PS_OR_PE, DIACRITIC
+
+
+def normalize(tokens):
+    normalized = []
+    for token in tokens:
+        # a diacritic that starts a word gets placed in a single token
+        if re.match(PS_OR_PE, token) or re.match(DIACRITIC, token):
+            continue
+        # diacritics
+        token = re.sub(DIACRITIC, '', token)
+        # dots
+        token = token.replace('ى', 'ي')\
+                     .replace('ة', 'ه')
+        # alef typing mistakes
+        token = re.sub(r'اا|ااا', 'ا', token)
+        # alef
+        token = re.sub(r'[أإآ]', 'ا', token)
+        normalized.append(token)
+    return normalized
+
+
+print(tokenize("(السلام) [عليكم] {ورحمة} الله"))
+print(normalize(tokenize("(السلام) [عليكم] {ورحمة} الله")))
+print(normalize(tokenize("{هذا} «يوسف» عبد الفتاح")))
+print(normalize(tokenize("أَهْلاً وسَهلاً بِكُم هلَّا")))
+print(normalize(tokenize("ىأآإئؤة")))
---
+++ tokenizer.py
@@ -0,0 +1,104 @@
+import re
+
+from constants import DIACRITIC
+
+
+class Tokenizer:
+    def __init__(self, text: str):
+        self.start = 0
+        self.current = 0
+        self.text = text
+        self.tokens = []
+
+    def consume(self):
+        self.current += 1
+        return self.cur_char()
+
+    def get(self, idx):
+        if idx >= len(self.text):
+            return None
+        return self.text[idx]
+
+    # note: can return None
+    def peek(self, amount):
+        return self.get(self.current + amount)
+
+    def pop(self):
+        self.tokens.append(self.text[self.start:self.current])
+        self.start = self.current
+
+    def skip(self):
+        self.consume()
+        self.start = self.current
+
+    def cur_char(self):
+        return self.get(self.current)
+
+    def at_end(self):
+        return self.current >= len(self.text)
+
+    def tokenize(self):
+        while True:
+            # whitespaces and newlines
+            cur = self.cur_char()
+            if cur is None:
+                break
+            if re.match('\\s', cur) is not None:
+                self.skip()
+            # words
+            elif cur.isalpha():
+                self.consume()
+                while True:
+                    cur = self.cur_char()
+                    if cur is None:
+                        break
+                    if not cur.isalpha() and re.match(DIACRITIC, cur) is None:
+                        break
+                    self.consume()
+                self.pop()
+            # numbers and floats
+            elif cur.isdigit():
+                self.consume()
+                while True:
+                    cur = self.cur_char()
+                    if cur is None:
+                        break
+                    if not cur.isdigit():
+                        break
+                    self.consume()
+                if cur == '.':
+                    following = self.peek(1)
+                    if following is not None and following.isdigit():
+                        cur = self.consume()  # '.'
+                        self.consume()  # consider omitting it
+                        while True:
+                            cur = self.cur_char()
+                            if cur is None:
+                                break
+                            if not cur.isdigit():
+                                break
+                            self.consume()
+                self.pop()
+            # floats starting with '.'
+            elif cur == '.':
+                cur = self.consume()
+                if cur is None:
+                    pass
+                elif cur.isdigit():
+                    while True:
+                        cur = self.cur_char()
+                        if cur is None:
+                            break
+                        if not cur.isdigit():
+                            break
+                        self.consume()
+                self.pop()
+                # punctuation marks
+            else:
+                self.consume()
+                self.pop()
+        return self.tokens
+
+
+def tokenize(text):
+    return Tokenizer(text).tokenize()
